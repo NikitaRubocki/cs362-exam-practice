19d577fdc66c67f7fab7fd722f582dbe129e8f74
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-17 16:41:06 -0700

    Add player to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,11 @@
+require_relative 'player'
+
 class Game
-    def initialize
-        
+
+    attr_accessor :player
+
+    def initialize(player= Player.new())
+        @player = player
     end
 
     def start
